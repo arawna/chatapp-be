1cf483dcb69eff6890cad197158caaf4e013b410
arawna <alihocaoglu1220@gmail.com>
2023-06-02 03:00:41 +0300

    test

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
-app.use("/users", require("./controllers/userController"))
+app.use("/api/users", require("./controllers/userController"))
 
 
 const server = http.createServer(app);
